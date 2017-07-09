821e801ac69063b973d190575d3325e420800b67
Ivan Blagoychev <ivanblagoychev@gmail.com>
2017-07-09 15:44:48 +0300

    Lab Tasks

---
+++ 07. Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,15 @@
+﻿public abstract class Animal
+{
+    public Animal(string name, string favouriteFood)
+    {
+        this.Name = name;
+        this.FavouriteFood = favouriteFood;
+    }
+    public string Name { get; protected set; }
+    public string FavouriteFood { get; protected set; }
+
+    public virtual string ExplainMyself()
+    {
+        return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+    }
+}
---
+++ 07. Polymorphism - Lab/03. Shapes/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+
+    public double Radius
+    {
+        get => this.radius;
+        set => this.radius = value;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * Math.PI * this.radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * this.radius * this.radius;
+    }
+
+    public sealed override string Draw()
+    {
+        return base.Draw() + "Circle";
+    }
+
+}
